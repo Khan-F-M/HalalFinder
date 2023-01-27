75731d334b36a0eb1b022e92ea92839383ccbdc6
Khan-F-M <$Faisal10>
2023-01-26 22:48:30 -0500

    Before adding React

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const path = require('path');
-const Sequelize = require('sequelize');
 const rest = require('./restaurantsdb.js')
-const exphbs = require('express-handlebars');
 
 const app = express();
 app.use(express.static('views'));
 const HTTP_PORT = process.env.PORT || 8080;
-
-/*******************************************************/
-app.set('views', path.join(__dirname, 'views'))
-
-const hbs = exphbs.create({
-    partialsDir: path.join(__dirname, 'views/partials')
-});
-
-app.engine('.hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
-/*******************************************************/
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Api Listening' })
