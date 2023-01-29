51b6778ee79c78f924702be70ad3c49f27170020
Khan-F-M <$Faisal10>
2023-01-29 08:24:32 -0500

    Fixed Fetch, Template Literal, and Data.Map

--- js/main.js
+++ js/main.js
@@ -15,24 +15,50 @@ function loadRestaurants() {
     fetch(`https://tan-gifted-cod.cyclic.app/places/${storedValue}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             let cardbod = `
-        ${data.data.map(rest => ( //WHY IS DATA.DATA.MAP working wth...
-                `<h5 class="card-title" id="restname">${rest.name}</h5>
-            <p class="card-text" id="restdesc">${rest.description}</p>`
+    ${data.data.map(rest => (
+                `<div class="col">
+                <div class="card" style="width: 18rem; margin-left: 2%; margin-right: 2%; margin-top: 1%;">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="images/Italian.jpg" class="d-block w-100" alt="..."
+                                    style=" height: 200px; width: 100%;">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/india.png" class="d-block w-100" alt="..."
+                                    style=" height: 200px; width: 100%;">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="images/Afghanistan.jpg" class="d-block w-100" alt="..."
+                                    style=" height: 200px; width: 100%;">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title" id="restname">${rest.name}</h5>
+                        <p class="card-text" id="restdesc">${rest.description}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" id="address"> ${rest.address}</li>
+                        <li class="list-group-item" id="phone">${rest.phone}</li>
+                        <li class="list-group-item" id="category">${rest.cu_id}</li>
+                    </ul>
+                </div>
+            </div>`
             )).join('')} 
-      `;
-
-            let listbod = `
-      ${data.data.map(rest => (
-                `<li class="list-group-item" id="address">${rest.address}</li>
-          <li class="list-group-item" id="phone">${rest.phone}</li>
-          <li class="list-group-item" id="category">${rest.cu_id}</li>`
-            )).join('')} 
-    `;
-
-            document.querySelector('.card-body').innerHTML = cardbod;
-            document.querySelector('#list-body').innerHTML = listbod;
+            `;
+            document.getElementById('crdbod').innerHTML = cardbod;
         })
         .catch(error => {
             console.log('Error fetching data: ', error);
